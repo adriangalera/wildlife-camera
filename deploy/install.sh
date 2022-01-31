3417ce2ca7adcc58e797cd45ff9a1c9e47863187
Adrian Galera <adrian.galera.87@gmail.com>
2022-01-31 21:49:15 +0100

    Bluetooth (#1)
    bluetooth install

--- deploy/install.sh
+++ deploy/install.sh
@@ -3,3 +3,11 @@
 printf "Installing motion and motioneye ...\n\n"
 chmod +x /home/pi/wild/deploy/motioneye-installer.sh
 /home/pi/wild/deploy/motioneye-installer.sh
+
+printf "Setting up the system to unblock all radios on startup ...\n\n"
+chmod +x /home/pi/wild/deploy/rfkill-unblock-all.sh
+/home/pi/wild/deploy/rfkill-unblock-all.sh
+
+printf "Installing bluetooth ...\n\n"
+chmod +x /home/pi/wild/deploy/bluetooth-installer.sh
+/home/pi/wild/deploy/bluetooth-installer.sh
